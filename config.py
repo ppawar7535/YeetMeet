da8845018c5fa10aa11dd4f70ff856880951c8ca
ppawar7535 <73441757+ppawar7535@users.noreply.github.com>
2020-11-05 12:59:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import os
+import os 
 class Config(object):
-	BOT_TOKEN = os.environ.get('1406378056:AAHjK7XkUCU_NrExrYv6BFkANrrLXzfO_84')
-	USERNAME = os.environ.get('ppawar7535@gmail.com')
-	PASSWORD = os.environ.get('Pranay@7535')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '1406378056:AAHjK7XkUCU_NrExrYv6BFkANrrLXzfO_84') 
+    USERNAME = os.environ.get('USERNAME', 'pranaypawar2019.it@mmcoe.edu.in') 
+    PASSWORD = os.environ.get('PASSWORD', 'Lucifer@2134819032')
